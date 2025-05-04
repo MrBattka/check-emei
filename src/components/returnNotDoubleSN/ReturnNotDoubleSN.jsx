72ffb53433355dfa7170e2887355a141fb8dab03
MrBaTTka <ilyha_96@inbox.ru>
2025-05-04 18:37:48 +0300

    fix

--- src/components/returnNotDoubleSN/ReturnNotDoubleSN.jsx
+++ src/components/returnNotDoubleSN/ReturnNotDoubleSN.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-import style from "./ReturnNotDoubleSN.module.css"
+import style from "./ReturnNotDoubleSN.module.css";
+import { copyTable } from "../../helpers/copy";
 
 const ReturnNotDoubleSN = ({ dataFromPhotoChange, dataBase }) => {
   const unique = dataFromPhotoChange.filter((a) => dataBase.indexOf(a) == -1);
+  console.log(unique);
 
   return (
     <div>
+      {/* <h3
+        onClick={() => {
+          copyTable();
+        }}
+      >
+        ❐ Copy
+      </h3> */}
       <h2 className={style.title}>Устаревшие S/N / IMEI:</h2>
-      <p className={style.p}>{unique}</p>
+      <p className={style.p}>
+        {unique.map((el) => (
+          <div>{el}</div>
+        ))}
+      </p>
     </div>
   );
 };
